ec0582f4e32e8ba3373a4447ab09a2aae6b9af70
HitMargin <hitmargin@qq.com>
2026-04-28 19:46:56 +0800

    build

--- app/src/main/obj/local/arm64-v8a/objs/October/Main.o.d
+++ app/src/main/obj/local/arm64-v8a/objs/October/Main.o.d
@@ -1,90 +1,90 @@
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/obj/local/arm64-v8a/objs/October/Main.o: \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/Main.cpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/universe.h \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/BasicMonoStructures.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/UserSettings/GlobalSettings.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/include/dobby.h \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/Il2CppHeaders.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/Il2CppHeaders/2022.2.h \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/DebugMessages.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/Utils.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/Defaults.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/Class.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/Image.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/ClassesManagement.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/ComplexMonoStructures.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/Method.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/MethodBase.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/Delegates.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/Coroutine.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/Event.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/EventBase.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/UnityStructures.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/UnityStructures/Color.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/UnityStructures/Quaternion.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/UnityStructures/Vector3.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/UnityStructures/Vector2.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/UnityStructures/Ray.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/UnityStructures/RaycastHit.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/UnityStructures/RaycastHit2D.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/UnityStructures/Rect.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/UnityStructures/Vector4.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/UnityStructures/Matrix3x3.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/UnityStructures/Matrix4x4.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/Exceptions.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/Field.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/FieldBase.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/src/private/Internals.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/Loading.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/Operators.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/Property.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/PropertyBase.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/UserSettings/Il2CppMethodNames.hpp \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/Enum/HitboxType.h \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/Enum/HitMargin.h \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/Enum/LevelEventType.h \
-  D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/Enum/DifficultyUIMode.h
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/universe.h:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/BasicMonoStructures.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/UserSettings/GlobalSettings.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/include/dobby.h:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/Il2CppHeaders.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/Il2CppHeaders/2022.2.h:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/DebugMessages.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/Utils.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/Defaults.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/Class.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/Image.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/ClassesManagement.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/ComplexMonoStructures.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/Method.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/MethodBase.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/Delegates.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/Coroutine.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/Event.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/EventBase.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/UnityStructures.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/UnityStructures/Color.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/UnityStructures/Quaternion.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/UnityStructures/Vector3.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/UnityStructures/Vector2.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/UnityStructures/Ray.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/UnityStructures/RaycastHit.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/UnityStructures/RaycastHit2D.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/UnityStructures/Rect.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/UnityStructures/Vector4.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/UnityStructures/Matrix3x3.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/UnityStructures/Matrix4x4.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/Exceptions.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/Field.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/FieldBase.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/src/private/Internals.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/Loading.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/Operators.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/Property.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/PropertyBase.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/BNM/include/BNM/UserSettings/Il2CppMethodNames.hpp:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/Enum/HitboxType.h:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/Enum/HitMargin.h:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/Enum/LevelEventType.h:
-D:/Projects/Android/ADOFAI2.9.5/app/src/main/jni/Enum/DifficultyUIMode.h:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/obj/local/arm64-v8a/objs/October/Main.o: \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/Main.cpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/universe.h \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/BasicMonoStructures.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/UserSettings/GlobalSettings.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/include/dobby.h \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/Il2CppHeaders.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/Il2CppHeaders/2022.2.h \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/DebugMessages.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/Utils.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/Defaults.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/Class.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/Image.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/ClassesManagement.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/ComplexMonoStructures.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/Method.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/MethodBase.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/Delegates.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/Coroutine.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/Event.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/EventBase.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/UnityStructures.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/UnityStructures/Color.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/UnityStructures/Quaternion.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/UnityStructures/Vector3.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/UnityStructures/Vector2.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/UnityStructures/Ray.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/UnityStructures/RaycastHit.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/UnityStructures/RaycastHit2D.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/UnityStructures/Rect.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/UnityStructures/Vector4.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/UnityStructures/Matrix3x3.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/UnityStructures/Matrix4x4.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/Exceptions.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/Field.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/FieldBase.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/src/private/Internals.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/Loading.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/Operators.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/Property.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/PropertyBase.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/UserSettings/Il2CppMethodNames.hpp \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/Enum/HitboxType.h \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/Enum/HitMargin.h \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/Enum/LevelEventType.h \
+  D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/Enum/DifficultyUIMode.h
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/universe.h:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/BasicMonoStructures.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/UserSettings/GlobalSettings.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/include/dobby.h:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/Il2CppHeaders.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/Il2CppHeaders/2022.2.h:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/DebugMessages.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/Utils.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/Defaults.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/Class.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/Image.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/ClassesManagement.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/ComplexMonoStructures.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/Method.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/MethodBase.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/Delegates.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/Coroutine.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/Event.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/EventBase.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/UnityStructures.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/UnityStructures/Color.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/UnityStructures/Quaternion.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/UnityStructures/Vector3.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/UnityStructures/Vector2.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/UnityStructures/Ray.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/UnityStructures/RaycastHit.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/UnityStructures/RaycastHit2D.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/UnityStructures/Rect.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/UnityStructures/Vector4.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/UnityStructures/Matrix3x3.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/UnityStructures/Matrix4x4.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/Exceptions.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/Field.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/FieldBase.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/src/private/Internals.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/Loading.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/Operators.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/Property.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/PropertyBase.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/BNM/include/BNM/UserSettings/Il2CppMethodNames.hpp:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/Enum/HitboxType.h:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/Enum/HitMargin.h:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/Enum/LevelEventType.h:
+D:/Projects/Android/ADOFAI-Mobile-Mod/app/src/main/jni/Enum/DifficultyUIMode.h:
